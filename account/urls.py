d8885af146cded40d27c729e3a891fb80a2d4bbe
Nurmurok <zhuzumalievanurmurok@gmail.com>
2022-10-09 05:33:02 +0600

    asd

--- account/urls.py
+++ account/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import (
 RegisterAPIView,
 MyObtainPairView,
-UserListView,
+UserListApiView,
 UserDetailApiView,
 UserDestroyApiView
 )
@@ -12,7 +12,7 @@ UserDestroyApiView
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),
     path('login/', MyObtainPairView.as_view(), name='login'),
-    path('list/', UserListView.as_view(), name='list'),
+    path('list/', UserListApiView.as_view(), name='list'),
     path('detail/', UserDetailApiView.as_view(), name='login'),
     path('delete/', UserDestroyApiView.as_view(), name='login'),
 ]
